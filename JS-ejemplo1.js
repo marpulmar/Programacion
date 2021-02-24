1bd3b0e607b2216c22e179004156da0a92f6c161
marpulmar <marinagpulgar@gmail.com>
2021-02-24 08:59:33 +0100

    Cambios

--- JS-ejemplo1.js
+++ JS-ejemplo1.js
@@ -1,12 +1,28 @@
-function edad () {
+//Ejemplo 1//
+function preguntar_edad () {
     alert("¡Hola!")
-    var numero = parseInt (prompt("¿Qué edad tienes? Introdúcela"))
-    var resultado;
-    resultado = parseInt (numero);
-    if (resultado > 17) {
+    var edad = parseInt (prompt("¿Qué edad tienes? Introdúcela"))
+    if (edad > 17) {
         alert ("Entonces eres mayor de edad")
     }
     else {
         alert ("Entonces eres menor de edad")
+    }
+}
+
+//Ejemplo 2//
+function preguntar_nota() {
+    var nota = (prompt ("¿Qué nota has sacado en el examen?"))
+    if (nota < 5) {
+        alert ("¡Oh vaya, has suspendido!")
+    }
+    else if (nota >= 5 && nota < 7) {
+        alert ("¡Estás aprobado!")
+    }
+    else if (nota >= 7 && nota < 9){
+        alert ("¡Tienes un notable!")
+    }
+    else {
+        alert ("¡Enhorabuena, tienes un sobresaliente!")
     }
 }
